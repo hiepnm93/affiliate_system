f22b8a8cdd80e999646b14ffc1ec4143a8e98e3a
hiepnm93 <hiepnm93@gmail.com>
2025-11-10 21:02:53 +0700

    fix(frontend): clean up imports and remove unused mock service worker code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,19 @@
 import "./global.css";
 import "./theme/theme.css";
 import "./locales/i18n";
-import ReactDOM from "react-dom/client";
+
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router";
+
 import App from "./App";
-// import { worker } from "./_mock";
+import ErrorBoundary from "./routes/components/error-boundary";
+import { GLOBAL_CONFIG } from "./global-config";
+import ReactDOM from "react-dom/client";
 import menuService from "./api/services/menuService";
 import { registerLocalIcons } from "./components/icon";
-import { GLOBAL_CONFIG } from "./global-config";
-import ErrorBoundary from "./routes/components/error-boundary";
 import { routesSection } from "./routes/sections";
-import { urlJoin } from "./utils";
 
 await registerLocalIcons();
-// await worker.start({
-// 	onUnhandledRequest: "bypass",
-// 	serviceWorker: { url: urlJoin(GLOBAL_CONFIG.publicPath, "mockServiceWorker.js") },
-// });
+
 if (GLOBAL_CONFIG.routerMode === "backend") {
 	await menuService.getMenuList();
 }
